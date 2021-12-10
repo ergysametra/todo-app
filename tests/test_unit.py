0629b87ada17d1e4be9c25a103986acf602d4cf7
ergysametra <93131954+ergysametra@users.noreply.github.com>
2021-12-10 11:47:05 +0000

    Update test_unit.py

--- tests/test_unit.py
+++ tests/test_unit.py
@@ -17,13 +17,12 @@ class TestBase(TestCase):
     def setUp(self):
         # Will be called before every test
         db.create_all() # create table schema
-
-        task = Tasks(description="Test the application")
-        db.session.add(task)
+        db.session.add(Tasks(description="Run unit tests"))
         db.session.commit()
 
     def tearDown(self):
         # Will be called after every test
+        db.session.remove()
         db.drop_all()
 
 class TestViews(TestBase):
@@ -35,28 +34,52 @@ class TestViews(TestBase):
     def test_create_task_get(self):
         response = self.client.get(url_for("create_task"))
         self.assert200(response)
+    def test_read_tasks(self):
+        response = self.client.get(url_for('read_task'))
+        self.assert200(response)
+    def test_update_task_get(self)
+        response = self.client.get(url_for('update_task', id=1))
         
 class TestRead(TestBase):
     
     def test_read_tasks(self):
         response = self.client.get(url_for("home"))
         self.assertIn("Test the application", str(response.data))
+    def tst_read_home_tasks(self):
+        response = self.client.get(url_for('read_tasks'))
+        self.assertIn(b"Run unit tests", response.data)
+        
 
 class TestCreate(TestBase):
 
     def test_create_task(self):
         response = self.client.post(
             url_for("create_task"),
-            json={"description": "Add a new task"},
+            data={"description": "Testing create functionallity"},
             follow_redirects=True
         )
-        new_task = Tasks.query.get(2)
-        self.assertEqual("Add a new task", new_task.description)
+        self.assertIn(b"Testing create functionallity", response.data)
+        
+ class TestUpdate(TestBase):
+    response =self.client.post(
+        url_for('update_task', id=1),
+        data={"description": "Testing update funcionality"},
+        follow_redirects=True
+    )
+    self.assertIn(b"Testing update functionallity", response.data)  
+    
+   def test_complete_task(self:
+     response =self.client.get(url_for('complete_task', id=1), follow_redirects=True)
+     self.assertEqual(Task.query.get(1).completed, True)
+                          
+                          
+ class TestDelete(Testbase):
+    
+    def test_delete_task(self:
+        response =self.client.get(
+            url_for('delete_task', id=1),
+            follow_redirects=True
+             )
+        self.assertIn(b"Run unit tests", response.data
 
-    def test_create_task_redirect(self):
-        response = self.client.post(
-            url_for("create_task"),
-            json={"description": "Add a new task"},
-            follow_redirects=True
-        )
-        self.assertIn("Add a new task", str(response.data))
+    
